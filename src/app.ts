8ee8f49e34fd8e8f07b51ac03958f32444d4e839
adi3009 <devcore3d@outlook.com>
2022-03-01 21:09:22 +0000

    use combineReducer to create rootReducer

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,10 @@
+import { combineReducers, createStore } from "redux";
 import { goals } from "./goals";
-import type { Goal } from "./goals";
-import { createStore } from "redux";
-import type { Action } from "redux";
 import { todos } from "./todos";
-import type { Todo } from "./todos";
 
-type State = { todos: Array<Todo>; goals: Array<Goal> };
-
-function appReducer(
-  state: State = { todos: [], goals: [] },
-  action: Action<string>
-): State {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  };
-}
+let appReducer = combineReducers({
+  todos,
+  goals,
+});
 
 export const store = createStore(appReducer);
